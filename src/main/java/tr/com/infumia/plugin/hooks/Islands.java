cccb6210ef6a208fe397381df206dcca3e3af138
Hasan Demirtaş <utsukushihito@outlook.com>
2021-05-01 16:31:53 +0300

    javadoc.

--- src/main/java/tr/com/infumia/plugin/hooks/Islands.java
+++ src/main/java/tr/com/infumia/plugin/hooks/Islands.java
@@ -10,6 +10,29 @@ import tr.com.infumia.plugin.hooks.hooks.BentoBoxWrapper;
 @UtilityClass
 public class Islands {
 
+  /**
+   * adds the level to the first island of the unique id.
+   *
+   * @param plugin the plugin to add.
+   * @param uniqueId the unique id to add.
+   * @param level the level to add.
+   */
+  public void addFirstIslandLevel(@NotNull final Plugin plugin,
+                                  @NotNull final UUID uniqueId, final long level) {
+    Hooks.getASkyBlock().ifPresentOrElse(wrapper ->
+      Islands.addFirstIslandLevel(wrapper, plugin, uniqueId, level), () ->
+      Hooks.getBentoBox().ifPresent(wrapper ->
+        Islands.addFirstIslandLevel(wrapper, plugin, uniqueId, level)));
+  }
+
+  /**
+   * adds the level to the first island of the unique id.
+   *
+   * @param wrapper the wrapper to add.
+   * @param plugin the plugin to add.
+   * @param uniqueId the unique id to add.
+   * @param level the level to add.
+   */
   public void addFirstIslandLevel(@NotNull final Wrapped wrapper, @NotNull final Plugin plugin,
                                   @NotNull final UUID uniqueId, final long level) {
     if (wrapper instanceof ASkyBlockWrapper) {
@@ -19,6 +42,31 @@ public class Islands {
     }
   }
 
+  /**
+   * gets the level of the first island of the unique id.
+   *
+   * @param uniqueId the unique id to get.
+   *
+   * @return level of the first island.
+   */
+  public long getFirstIslandLevel(@NotNull final UUID uniqueId) {
+    return Hooks.getASkyBlock()
+      .map(wrapper ->
+        Islands.getFirstIslandLevel(wrapper, uniqueId))
+      .orElse(Hooks.getBentoBox()
+        .map(wrapper ->
+          Islands.getFirstIslandLevel(wrapper, uniqueId))
+        .orElse(0L));
+  }
+
+  /**
+   * gets the level of the first island of the unique id.
+   *
+   * @param wrapper the wrapper to get.
+   * @param uniqueId the unique id to get.
+   *
+   * @return level of the first island.
+   */
   public long getFirstIslandLevel(@NotNull final Wrapped wrapper, @NotNull final UUID uniqueId) {
     if (wrapper instanceof ASkyBlockWrapper) {
       return ((ASkyBlockWrapper) wrapper).getIslandLevel(uniqueId);
--- src/main/java/tr/com/infumia/plugin/hooks/Wrapped.java
+++ src/main/java/tr/com/infumia/plugin/hooks/Wrapped.java
@@ -1,5 +1,8 @@
 package tr.com.infumia.plugin.hooks;
 
+/**
+ * a marker interface.
+ */
 public interface Wrapped {
 
 }
