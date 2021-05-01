a074e815476d44a52ede1325b1a1b88133be4121
Hasan Demirtaş <utsukushihito@outlook.com>
2021-05-01 16:24:23 +0300

    Update TaskUtilities.java

--- src/main/java/tr/com/infumia/plugin/utils/TaskUtilities.java
+++ src/main/java/tr/com/infumia/plugin/utils/TaskUtilities.java
@@ -13,78 +13,172 @@ import org.bukkit.scheduler.BukkitTask;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * a class that contains utility methods for {@link BukkitTask}.
+ */
 @UtilityClass
 public class TaskUtilities {
 
+  /**
+   * the plugin.
+   */
   @Nullable
   private Plugin plugin;
 
+  /**
+   * runs the given job.
+   *
+   * @param job the job to run.
+   *
+   * @return a newly run bukkit task instance.
+   */
   @NotNull
-  public BukkitTask async(@NotNull final Runnable runnable) {
+  public BukkitTask async(@NotNull final Runnable job) {
     return new BukkitRunnable() {
       @Override
       public void run() {
-        runnable.run();
+        job.run();
       }
     }.runTaskAsynchronously(TaskUtilities.getPlugin());
   }
 
+  /**
+   * runs the given job with a delay.
+   *
+   * @param delay the delay to run.
+   * @param job the job to run.
+   *
+   * @return a newly run bukkit task instance.
+   */
   @NotNull
-  public BukkitTask asyncLater(final long delay, @NotNull final Runnable runnable) {
+  public BukkitTask asyncLater(final long delay, @NotNull final Runnable job) {
     return new BukkitRunnable() {
       @Override
       public void run() {
-        runnable.run();
+        job.run();
       }
     }.runTaskLaterAsynchronously(TaskUtilities.getPlugin(), delay);
   }
 
+  /**
+   * runs the given job with a period.
+   *
+   * @param period the period to run.
+   * @param job the job to run.
+   *
+   * @return a newly run bukkit task instance.
+   */
   @NotNull
-  public BukkitTask asyncTimer(final long period, @NotNull final Runnable runnable) {
-    return TaskUtilities.asyncTimerLater(0L, period, runnable);
+  public BukkitTask asyncTimer(final long period, @NotNull final Runnable job) {
+    return TaskUtilities.asyncTimerLater(0L, period, job);
   }
 
+  /**
+   * runs the given job with a period, then if the job returns {@code false} stops the task.
+   *
+   * @param period the period to run.
+   * @param job the job to run.
+   *
+   * @return a newly run bukkit task instance.
+   */
   @NotNull
-  public BukkitTask asyncTimer(final long period, @NotNull final Supplier<Boolean> supplier) {
-    return TaskUtilities.asyncTimerLater(0L, period, supplier);
+  public BukkitTask asyncTimer(final long period, @NotNull final Supplier<Boolean> job) {
+    return TaskUtilities.asyncTimerLater(0L, period, job);
   }
 
+  /**
+   * runs the given job with a delay and period, then if the job returns {@code false} stops the task.
+   *
+   * @param delay the delay to run.
+   * @param period the period to run.
+   * @param job the job to run.
+   *
+   * @return a newly run bukkit task instance.
+   */
   @NotNull
-  public BukkitTask asyncTimerLater(final long delay, final long period, @NotNull final Supplier<Boolean> supplier) {
+  public BukkitTask asyncTimerLater(final long delay, final long period, @NotNull final Supplier<Boolean> job) {
     return new BukkitRunnable() {
       @Override
       public void run() {
-        if (!supplier.get()) {
+        if (!job.get()) {
           this.cancel();
         }
       }
     }.runTaskTimerAsynchronously(TaskUtilities.getPlugin(), delay, period);
   }
 
+  /**
+   * runs the given job with a delay and period.
+   *
+   * @param delay the delay to run.
+   * @param period the period to run.
+   * @param job the job to run.
+   *
+   * @return a newly run bukkit task instance.
+   */
   @NotNull
-  public BukkitTask asyncTimerLater(final long delay, final long period, @NotNull final Runnable runnable) {
+  public BukkitTask asyncTimerLater(final long delay, final long period, @NotNull final Runnable job) {
     return new BukkitRunnable() {
       @Override
       public void run() {
-        runnable.run();
+        job.run();
       }
     }.runTaskTimerAsynchronously(TaskUtilities.getPlugin(), delay, period);
   }
 
-  @NotNull
-  public <T> BukkitTask forAll(@NotNull final List<T> objects, @NotNull final Consumer<T> runArgument) {
-    return TaskUtilities.forAll(1, objects, runArgument);
+  /**
+   * initiates the task utilities.
+   *
+   * @param plugin the plugin to initiate.
+   */
+  public void init(@NotNull final Plugin plugin) {
+    TaskUtilities.plugin = plugin;
   }
 
+  /**
+   * runs the given job for all objects.
+   *
+   * @param objects the objects to run for all.
+   * @param job the run argument to run for all.
+   * @param <T> type of the object.
+   *
+   * @return a newly run bukkit task instance.
+   */
   @NotNull
-  public <T> BukkitTask forAll(final int perTick, final List<T> objects, @NotNull final Consumer<T> runArgument) {
-    return TaskUtilities.forAll(perTick, objects, runArgument, () -> {
+  public <T> BukkitTask runForAll(@NotNull final List<T> objects, @NotNull final Consumer<T> job) {
+    return TaskUtilities.runForAll(1, objects, job);
+  }
+
+  /**
+   * runs the given job for all objects.
+   *
+   * @param perTick the per tick to run for all.
+   * @param objects the objects to run for all.
+   * @param job the run argument to run for all.
+   * @param <T> type of the object.
+   *
+   * @return a newly run bukkit task instance.
+   */
+  @NotNull
+  public <T> BukkitTask runForAll(final int perTick, final List<T> objects, @NotNull final Consumer<T> job) {
+    return TaskUtilities.runForAll(perTick, objects, job, () -> {
     });
   }
 
+  /**
+   * runs the given job for all objects.
+   *
+   * @param perTick the per tick to run for all.
+   * @param objects the objects to run for all.
+   * @param job the run argument to run for all.
+   * @param onDone the on done to run for all.
+   * @param <T> type of the object.
+   *
+   * @return a newly run bukkit task instance.
+   */
   @NotNull
-  public <T> BukkitTask forAll(final int perTick, @NotNull final List<T> objects,
-                               @NotNull final Consumer<T> runArgument, @NotNull final Runnable onDone) {
+  public <T> BukkitTask runForAll(final int perTick, @NotNull final List<T> objects, @NotNull final Consumer<T> job,
+                                  @NotNull final Runnable onDone) {
     return new BukkitRunnable() {
       private int current = 0;
 
@@ -95,7 +189,7 @@ public class TaskUtilities {
           .mapToObj(i -> objects.get(this.current))
           .forEach(object -> {
             try {
-              runArgument.accept(object);
+              job.accept(object);
             } catch (final RuntimeException e) {
               TaskUtilities.getPlugin().getLogger().log(
                 Level.SEVERE,
@@ -113,62 +207,114 @@ public class TaskUtilities {
     }.runTaskTimer(TaskUtilities.getPlugin(), 1L, 1L);
   }
 
-  public void init(@NotNull final Plugin plugin) {
-    TaskUtilities.plugin = plugin;
-  }
-
+  /**
+   * runs the given job.
+   *
+   * @param job the job to run.
+   *
+   * @return a newly run bukkit task instance.
+   */
   @NotNull
-  public BukkitTask sync(@NotNull final Runnable runnable) {
+  public BukkitTask sync(@NotNull final Runnable job) {
     return new BukkitRunnable() {
       @Override
       public void run() {
-        runnable.run();
+        job.run();
       }
     }.runTask(TaskUtilities.getPlugin());
   }
 
+  /**
+   * runs the given job with a delay.
+   *
+   * @param delay the delay to run.
+   * @param job the job to run.
+   *
+   * @return a newly run bukkit task instance.
+   */
   @NotNull
-  public BukkitTask syncLater(final long delay, @NotNull final Runnable runnable) {
+  public BukkitTask syncLater(final long delay, @NotNull final Runnable job) {
     return new BukkitRunnable() {
       @Override
       public void run() {
-        runnable.run();
+        job.run();
       }
     }.runTaskLater(TaskUtilities.getPlugin(), delay);
   }
 
+  /**
+   * runs the job with a period.
+   *
+   * @param period the period to run.
+   * @param job the job to run.
+   *
+   * @return a newly run bukkit task instance.
+   */
   @NotNull
-  public BukkitTask syncTimer(final long period, @NotNull final Runnable runnable) {
-    return TaskUtilities.syncTimerLater(0L, period, runnable);
+  public BukkitTask syncTimer(final long period, @NotNull final Runnable job) {
+    return TaskUtilities.syncTimerLater(0L, period, job);
   }
 
+  /**
+   * runs the job with a period, then if the job returns {@code false} stops the task.
+   *
+   * @param period the period to run.
+   * @param job the job to run.
+   *
+   * @return a newly run bukkit task instance.
+   */
   @NotNull
-  public BukkitTask syncTimer(final long period, @NotNull final Supplier<Boolean> supplier) {
-    return TaskUtilities.syncTimerLater(0L, period, supplier);
+  public BukkitTask syncTimer(final long period, @NotNull final Supplier<Boolean> job) {
+    return TaskUtilities.syncTimerLater(0L, period, job);
   }
 
+  /**
+   * runs the given job with a delay and period, then if the job returns {@code false} stops the task.
+   *
+   * @param delay the delay to run.
+   * @param period the period to run.
+   * @param job the job to run.
+   *
+   * @return a newly run bukkit task instance.
+   */
   @NotNull
-  public BukkitTask syncTimerLater(final long delay, final long period, @NotNull final Supplier<Boolean> supplier) {
+  public BukkitTask syncTimerLater(final long delay, final long period, @NotNull final Supplier<Boolean> job) {
     return new BukkitRunnable() {
       @Override
       public void run() {
-        if (!supplier.get()) {
+        if (!job.get()) {
           this.cancel();
         }
       }
     }.runTaskTimer(TaskUtilities.getPlugin(), delay, period);
   }
 
+  /**
+   * runs the given job with a delay and period.
+   *
+   * @param delay the delay to run.
+   * @param period the period to run.
+   * @param job the job to run.
+   *
+   * @return a newly run bukkit task instance.
+   */
   @NotNull
-  public BukkitTask syncTimerLater(final long delay, final long period, @NotNull final Runnable runnable) {
+  public BukkitTask syncTimerLater(final long delay, final long period, @NotNull final Runnable job) {
     return new BukkitRunnable() {
       @Override
       public void run() {
-        runnable.run();
+        job.run();
       }
     }.runTaskTimer(TaskUtilities.getPlugin(), delay, period);
   }
 
+  /**
+   * obtains the plugin.
+   *
+   * @return plugin.
+   *
+   * @throws RuntimeException if {@link #plugin} is null.
+   */
   @NotNull
   private Plugin getPlugin() {
     return Optional.ofNullable(TaskUtilities.plugin)
