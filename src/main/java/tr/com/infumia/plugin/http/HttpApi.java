90673f2a7ee1b016994c7fe38f48848f1076dce3
Hasan Demirtaş <utsukushihito@outlook.com>
2021-05-07 16:10:48 +0300

    separate into async and sync api

--- src/main/java/tr/com/infumia/plugin/http/HttpApi.java
+++ src/main/java/tr/com/infumia/plugin/http/HttpApi.java
@@ -1,10 +1,10 @@
 package tr.com.infumia.plugin.http;
 
 import com.google.protobuf.GeneratedMessageV3;
-import java.util.concurrent.CompletableFuture;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class HttpApi {
@@ -15,27 +15,25 @@ public abstract class HttpApi {
   @NotNull
   private final String path;
 
-  @NotNull
-  protected final CompletableFuture<Boolean> delete(@NotNull final String path) {
-    return CompletableFuture.supplyAsync(() -> ProtobufHttp.create().delete(this.path(path)));
+  protected final boolean delete(@NotNull final String path) {
+    return ProtobufHttp.create().delete(this.path(path));
   }
 
-  @NotNull
-  protected final <T extends GeneratedMessageV3> CompletableFuture<T> get(@NotNull final String path,
-                                                                          @NotNull final T defaultInstance) {
-    return CompletableFuture.supplyAsync(() -> ProtobufHttp.create().get(this.path(path), defaultInstance));
+  @Nullable
+  protected final <T extends GeneratedMessageV3> T get(@NotNull final String path, @NotNull final T defaultInstance) {
+    return ProtobufHttp.create().get(this.path(path), defaultInstance);
   }
 
-  @NotNull
-  protected final <T extends GeneratedMessageV3> CompletableFuture<Boolean> post(@NotNull final String path,
-                                                                                 @NotNull final T defaultInstance) {
-    return CompletableFuture.supplyAsync(() -> ProtobufHttp.create().post(this.path(path), defaultInstance));
+  protected final <T extends GeneratedMessageV3> boolean post(@NotNull final String path,
+                                                              @NotNull final T defaultInstance) {
+    return ProtobufHttp.create().post(this.path(path), defaultInstance);
   }
 
-  @NotNull
-  protected final <T extends GeneratedMessageV3, B extends GeneratedMessageV3> CompletableFuture<T> post(
-    @NotNull final String path, @NotNull final T defaultInstance, @NotNull final B body) {
-    return CompletableFuture.supplyAsync(() -> ProtobufHttp.create().post(this.path(path), defaultInstance, body));
+  @Nullable
+  protected final <T extends GeneratedMessageV3, B extends GeneratedMessageV3> T post(@NotNull final String path,
+                                                                                      @NotNull final T defaultInstance,
+                                                                                      @NotNull final B body) {
+    return ProtobufHttp.create().post(this.path(path), defaultInstance, body);
   }
 
   private String path(@NotNull final String s) {
