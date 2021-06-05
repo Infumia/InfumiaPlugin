176c193e9a3a24131329ddc2da0377c462036380
Hasan Demirtaş <utsukushihito@outlook.com>
2021-06-05 15:57:23 +0300

    number util.
    effective permission.

--- paper/src/main/java/tr/com/infumia/infumialib/paper/hooks/Groups.java
+++ paper/src/main/java/tr/com/infumia/infumialib/paper/hooks/Groups.java
@@ -1,20 +1,44 @@
 package tr.com.infumia.infumialib.paper.hooks;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 import lombok.experimental.UtilityClass;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.PermissionAttachmentInfo;
 import org.jetbrains.annotations.NotNull;
 import tr.com.infumia.infumialib.hooks.Wrapped;
 import tr.com.infumia.infumialib.paper.hooks.hooks.GroupManagerWrapper;
 import tr.com.infumia.infumialib.paper.hooks.hooks.LuckPermsWrapper;
 import tr.com.infumia.infumialib.paper.hooks.hooks.PermissionsExWrapper;
+import tr.com.infumia.infumialib.paper.utils.NumberUtil;
 
 /**
  * a class that contains utility methods for group/permission plugins.
  */
 @UtilityClass
 public class Groups {
+
+  /**
+   * calculates the given permission's limit.
+   *
+   * @param permission the permission to calculate.
+   * @param permissions the permissions to calculate.
+   * @param calculatedLimit the calculated limit.
+   */
+  public void calculatePermissionLimit(@NotNull final String permission, @NotNull final List<String> permissions,
+                                       @NotNull final AtomicLong calculatedLimit) {
+    permissions.stream()
+      .filter(perm -> !perm.startsWith("-"))
+      .filter(perm -> perm.startsWith(permission))
+      .map(perm -> perm.substring(perm.lastIndexOf(".") + 1))
+      .filter(NumberUtil::isLong)
+      .mapToLong(Integer::parseInt)
+      .filter(limit -> limit > calculatedLimit.get())
+      .forEach(calculatedLimit::set);
+  }
 
   /**
    * checks if the given player has the given group.
@@ -67,24 +91,60 @@ public class Groups {
   }
 
   /**
-   * gets the player's first group.
+   * gets player's limit in the given permission.
+   * <p>
+   * permission pattern should be like 'xxx.yyy.zzz.'
    *
+   * @param permission the permission to get.
    * @param player the player to get.
+   * @param defaultValue the default value to get.
    *
-   * @return player's first group.
+   * @return player's limit in the permission.
    */
-  @NotNull
-  public Optional<String> getFirstGroup(@NotNull final Player player) {
-    return Optional.ofNullable(Hooks.getGroupManager()
-      .flatMap(wrapper ->
-        Groups.getFirstGroup(wrapper, player))
+  public long getEffectiveLimitedPermission(@NotNull final String permission,
+                                            @NotNull final Player player, final long defaultValue) {
+    return Hooks.getGroupManager()
+      .map(wrapper ->
+        Groups.getEffectiveLimitedPermission(wrapper, permission, player, defaultValue))
       .orElse(Hooks.getLuckPerms()
-        .flatMap(wrapper ->
-          Groups.getFirstGroup(wrapper, player))
+        .map(wrapper ->
+          Groups.getEffectiveLimitedPermission(wrapper, permission, player, defaultValue))
         .orElse(Hooks.getPermissionsEx()
-          .flatMap(wrapper ->
-            Groups.getFirstGroup(wrapper, player))
-          .orElse(null))));
+          .map(wrapper ->
+            Groups.getEffectiveLimitedPermission(wrapper, permission, player, defaultValue))
+          .orElse(defaultValue)));
+  }
+
+  /**
+   * gets player's limit in the given permission.
+   * <p>
+   * permission pattern should be like 'xxx.yyy.zzz.'
+   *
+   * @param wrapper the wrapper to get.
+   * @param permission the permission to get.
+   * @param player the player to get.
+   * @param defaultValue the default value to get.
+   *
+   * @return player's limit in the permission.
+   */
+  public long getEffectiveLimitedPermission(@NotNull final Wrapped wrapper, @NotNull final String permission,
+                                            @NotNull final Player player, final long defaultValue) {
+    if (wrapper instanceof GroupManagerWrapper) {
+      return ((GroupManagerWrapper) wrapper).getEffectiveLimitedPermission(permission, player, defaultValue);
+    }
+    if (wrapper instanceof LuckPermsWrapper) {
+      return ((LuckPermsWrapper) wrapper).getEffectiveLimitedPermission(permission, player, defaultValue);
+    }
+    if (wrapper instanceof PermissionsExWrapper) {
+      return ((PermissionsExWrapper) wrapper).getEffectiveLimitedPermission(permission, player, defaultValue);
+    }
+    final var calculatedLimit = new AtomicLong(defaultValue);
+    final var permissions = player.getEffectivePermissions().stream()
+      .filter(PermissionAttachmentInfo::getValue)
+      .map(PermissionAttachmentInfo::getPermission)
+      .collect(Collectors.toList());
+    Groups.calculatePermissionLimit(permission, permissions, calculatedLimit);
+    return calculatedLimit.get();
   }
 
   /**
@@ -109,5 +169,26 @@ public class Groups {
         .map(strings -> strings.get(0));
     }
     return Optional.empty();
+  }
+
+  /**
+   * gets the player's first group.
+   *
+   * @param player the player to get.
+   *
+   * @return player's first group.
+   */
+  @NotNull
+  public Optional<String> getFirstGroup(@NotNull final Player player) {
+    return Optional.ofNullable(Hooks.getGroupManager()
+      .flatMap(wrapper ->
+        Groups.getFirstGroup(wrapper, player))
+      .orElse(Hooks.getLuckPerms()
+        .flatMap(wrapper ->
+          Groups.getFirstGroup(wrapper, player))
+        .orElse(Hooks.getPermissionsEx()
+          .flatMap(wrapper ->
+            Groups.getFirstGroup(wrapper, player))
+          .orElse(null))));
   }
 }
--- paper/src/main/java/tr/com/infumia/infumialib/paper/hooks/hooks/PermissionsExWrapper.java
+++ paper/src/main/java/tr/com/infumia/infumialib/paper/hooks/hooks/PermissionsExWrapper.java
@@ -2,17 +2,46 @@ package tr.com.infumia.infumialib.paper.hooks.hooks;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import ru.tehkode.permissions.bukkit.PermissionsEx;
 import tr.com.infumia.infumialib.hooks.Wrapped;
+import tr.com.infumia.infumialib.paper.hooks.Groups;
 
 @RequiredArgsConstructor
 public final class PermissionsExWrapper implements Wrapped {
 
   @NotNull
   private final PermissionsEx permissionsEx;
+
+  /**
+   * gets player's limit in the given permission.
+   * <p>
+   * permission pattern should be like 'xxx.yyy.zzz.'
+   *
+   * @param permission the permission to get.
+   * @param player the player to get.
+   * @param defaultValue the default value to get.
+   *
+   * @return player's limit in the permission.
+   */
+  public long getEffectiveLimitedPermission(@NotNull final String permission, @NotNull final Player player,
+                                            final long defaultValue) {
+    final var calculatedLimit = new AtomicLong(defaultValue);
+    final var user = this.permissionsEx.getPermissionsManager().getUser(player.getUniqueId());
+    final var permissionMap = user.getAllPermissions();
+    final var permissions = permissionMap.entrySet().stream()
+      .flatMap(entry -> entry.getValue().stream())
+      .collect(Collectors.toList());
+    if (permissions.isEmpty()) {
+      return calculatedLimit.get();
+    }
+    Groups.calculatePermissionLimit(permission, permissions, calculatedLimit);
+    return calculatedLimit.get();
+  }
 
   @NotNull
   public Optional<String> getGroupPrefix(@NotNull final String world, @NotNull final String group) {
