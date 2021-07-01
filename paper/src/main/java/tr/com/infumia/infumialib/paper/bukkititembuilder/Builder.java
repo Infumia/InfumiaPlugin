e13fb002d6622e0c970a71378a5357dc2a1c5b66
Hasan Demirtaş <utsukushihito@outlook.com>
2021-07-01 19:06:58 +0300

    fix.

--- paper/src/main/java/tr/com/infumia/infumialib/paper/bukkititembuilder/Builder.java
+++ paper/src/main/java/tr/com/infumia/infumialib/paper/bukkititembuilder/Builder.java
@@ -525,7 +525,8 @@ public abstract class Builder<X extends Builder<X, T>, T extends ItemMeta> imple
   @NotNull
   @Override
   public final ItemStack getItemStack(final boolean update) {
-    if (update) {
+    if (update &&
+      !Objects.equals(this.itemMeta, this.itemStack.getItemMeta())) {
       this.itemStack.setItemMeta(this.itemMeta);
     }
     return this.itemStack;
