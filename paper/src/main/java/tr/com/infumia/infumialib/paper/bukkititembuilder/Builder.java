68efbef1ef913b989667d8b44bf08160af93d74d
Hasan Demirtaş <utsukushihito@outlook.com>
2021-07-01 17:06:43 +0300

    Revert "revert"
    This reverts commit f6a11adb1ea9d212729a9b45530e3bb9ff6a6572.

--- paper/src/main/java/tr/com/infumia/infumialib/paper/bukkititembuilder/Builder.java
+++ paper/src/main/java/tr/com/infumia/infumialib/paper/bukkititembuilder/Builder.java
@@ -525,7 +525,8 @@ public abstract class Builder<X extends Builder<X, T>, T extends ItemMeta> imple
   @NotNull
   @Override
   public final ItemStack getItemStack(final boolean update) {
-    if (update) {
+    if (update &&
+      !Objects.equals(this.itemStack.getItemMeta(), this.itemMeta)) {
       this.itemStack.setItemMeta(this.itemMeta);
     }
     return this.itemStack;
