6cfcc93cc0a35f718df8d21b40ad1e47a1ce3ba9
Hasan Demirtaş <utsukushihito@outlook.com>
2021-07-14 01:40:13 +0300

    make it second.

--- common/src/main/java/tr/com/infumia/infumialib/time/Times.java
+++ common/src/main/java/tr/com/infumia/infumialib/time/Times.java
@@ -6,17 +6,17 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Times {
 
-  private final long ONE_HOUR = Times.ONE_DAY / 24;
+  private final long ONE_HOUR = Times.ONE_DAY / 24L;
 
-  private final long ONE_MINUTE = Times.ONE_HOUR / 60;
+  private final long ONE_MINUTE = Times.ONE_HOUR / 60L;
 
-  private final long ONE_SECOND = Times.ONE_MINUTE / 60;
+  private final long ONE_SECOND = Times.ONE_MINUTE / 60L;
 
-  private final long ONE_YEAR = 1000L * 60L * 60L * 24L * 30L * 12L;
+  private final long ONE_YEAR = 60L * 60L * 24L * 30L * 12L;
 
-  private final long ONE_MONTH = Times.ONE_YEAR / 12;
+  private final long ONE_MONTH = Times.ONE_YEAR / 12L;
 
-  private final long ONE_DAY = Times.ONE_MONTH / 30;
+  private final long ONE_DAY = Times.ONE_MONTH / 30L;
 
   public long byDay(final long time) {
     return TimeUnit.SECONDS.toDays(time);
@@ -56,14 +56,12 @@ public class Times {
     final var minute = tempDiff / Times.ONE_MINUTE;
     tempDiff %= Times.ONE_MINUTE;
     final var second = tempDiff / Times.ONE_SECOND;
-    final var millisecond = tempDiff % Times.ONE_SECOND;
     result[0] = year;
     result[1] = month;
     result[2] = day;
     result[3] = hour;
     result[4] = minute;
     result[5] = second;
-    result[6] = millisecond;
     return result;
   }
 }
