8768e003b5746e10be8c896e1941b8f9f4d37cb3
Hasan Demirtaş <utsukushihito@outlook.com>
2021-07-14 01:34:18 +0300

    difference.

--- common/src/main/java/tr/com/infumia/infumialib/time/Times.java
+++ common/src/main/java/tr/com/infumia/infumialib/time/Times.java
@@ -6,6 +6,18 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Times {
 
+  private final long ONE_HOUR = Times.ONE_DAY / 24;
+
+  private final long ONE_MINUTE = Times.ONE_HOUR / 60;
+
+  private final long ONE_SECOND = Times.ONE_MINUTE / 60;
+
+  private final long ONE_YEAR = 1000L * 60L * 60L * 24L * 30L * 12L;
+
+  private final long ONE_MONTH = Times.ONE_YEAR / 12;
+
+  private final long ONE_DAY = Times.ONE_MONTH / 30;
+
   public long byDay(final long time) {
     return TimeUnit.SECONDS.toDays(time);
   }
@@ -28,5 +40,30 @@ public class Times {
 
   public long byYear(final long time) {
     return time / 31536000;
+  }
+
+  public static long[] getTimeDifference(final long difference) {
+    var tempDiff = difference;
+    final var result = new long[7];
+    final var year = tempDiff / Times.ONE_YEAR;
+    tempDiff %= Times.ONE_YEAR;
+    final var month = tempDiff / Times.ONE_MONTH;
+    tempDiff %= Times.ONE_MONTH;
+    final var day = tempDiff / Times.ONE_DAY;
+    tempDiff %= Times.ONE_DAY;
+    final var hour = tempDiff / Times.ONE_HOUR;
+    tempDiff %= Times.ONE_HOUR;
+    final var minute = tempDiff / Times.ONE_MINUTE;
+    tempDiff %= Times.ONE_MINUTE;
+    final var second = tempDiff / Times.ONE_SECOND;
+    final var millisecond = tempDiff % Times.ONE_SECOND;
+    result[0] = year;
+    result[1] = month;
+    result[2] = day;
+    result[3] = hour;
+    result[4] = minute;
+    result[5] = second;
+    result[6] = millisecond;
+    return result;
   }
 }
